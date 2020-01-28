6e5fcb58e470ce7265842636664d8104ead8542a
subha2125 <subha2125@gmail.com>
2020-01-28 15:00:06 +0530

    Adding password encoder and method to get User

--- employee-details-service/src/main/java/com/ericcsson/employeedetailsservice/controller/EmployeeAddController.java
+++ employee-details-service/src/main/java/com/ericcsson/employeedetailsservice/controller/EmployeeAddController.java
@@ -1,8 +1,12 @@
 package com.ericcsson.employeedetailsservice.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +31,7 @@ public class EmployeeAddController {
 	@CrossOrigin("*")
 	@PostMapping("/saveUser")
 	public @ResponseBody User saveUser(@RequestBody User user) {
+		user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
 		return userRepository.save(user);
 	}
 	
@@ -34,6 +39,18 @@ public class EmployeeAddController {
 	@PostMapping("/saveRole")
 	public @ResponseBody Role saveRole(@RequestBody Role role) {
 		return roleRepository.save(role);
+	}
+	
+	@CrossOrigin("*")
+	@GetMapping("/getAllUser")
+	public List<User> getAllUser() {
+		return userRepository.findAll();
+	}
+	
+	@CrossOrigin("*")
+	@GetMapping("/getUserByName/{username}")
+	public User getUserByName(@PathVariable String username) {
+		return userRepository.findByUsername(username);
 	}
 	
 	@GetMapping("/hello")
