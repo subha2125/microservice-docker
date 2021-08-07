941a92897569791f9600915033829a1910c25a2b
Subhadip Ghosh <subhadip.ghosh@ericsson.com>
2021-08-07 11:13:06 +0530

    Added Separate Kafka topic for Micro service

--- employee-info-service/src/main/java/com/ericcsson/employeeinfoservice/repository/EmployeeInfoRepository.java
+++ employee-info-service/src/main/java/com/ericcsson/employeeinfoservice/repository/EmployeeInfoRepository.java
@@ -1,9 +1,11 @@
 package com.ericcsson.employeeinfoservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ericcsson.employeeinfoservice.model.EmployeeInfo;
 
+@Repository
 public interface EmployeeInfoRepository extends JpaRepository<EmployeeInfo, Long>{
 
 }
