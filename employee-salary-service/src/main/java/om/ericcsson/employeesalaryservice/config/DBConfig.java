ea0ad2911f927b7d3e07382efa593d0e8daf92f4
Subhadip Ghosh <subhadip.ghosh@ericsson.com>
2021-08-12 19:28:25 +0530

    Added Custom dataSource Bean, JPA properties read from config-server

--- employee-salary-service/src/main/java/om/ericcsson/employeesalaryservice/config/DBConfig.java
+++ employee-salary-service/src/main/java/om/ericcsson/employeesalaryservice/config/DBConfig.java
@@ -1,46 +1,46 @@
 package om.ericcsson.employeesalaryservice.config;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(entityManagerFactoryRef = "entityManagerFactory",
+    basePackages = {"com.ericcsson.employeesalaryservice.model"})
 public class DBConfig {
 
-	/*@Value("${spring.datasource.url}")
-	private String url;
-	@Value("${spring.datasource.username}")
-	private String userName;
-	@Value("${spring.datasource.password}")
-	private String password;
-	@Value("${spring.datasource.driver-class-name}")
-	private String driverClass;
+	@Primary
+	  @Bean(name = "dataSource")
+	  @ConfigurationProperties(prefix = "spring.datasource")
+	  public DataSource dataSource() {
+	    return DataSourceBuilder.create().build();
+	  }
 
-	@Bean
-	public DataSource dataSource() {
-		DataSourceBuilder builder = DataSourceBuilder.create();
-		builder.url(url);
-		builder.username(userName);
-		builder.password(password);
-		builder.driverClassName(driverClass);
-		return builder.build();
-	}
-	
-	@Bean
-	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-	      LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-	      em.setDataSource(dataSource());
-	      em.setPackagesToScan("com.ericcsson.employeesalaryservice.model");
+	  @Primary
+	  @Bean(name = "entityManagerFactory")
+	  public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+	      EntityManagerFactoryBuilder builder, @Qualifier("dataSource") DataSource dataSource) {
+	    return builder.dataSource(dataSource).packages("com.ericcsson.employeesalaryservice.model").persistenceUnit("EmployeeSalary")
+	        .build();
+	  }
 
-	      JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-	      em.setJpaVendorAdapter(vendorAdapter);
-
-	      return em;
-	}*/
+	  @Primary
+	  @Bean(name = "transactionManager")
+	  public PlatformTransactionManager transactionManager(
+	      @Qualifier("entityManagerFactory") EntityManagerFactory entityManagerFactory) {
+	    return new JpaTransactionManager(entityManagerFactory);
+	  }
 }
